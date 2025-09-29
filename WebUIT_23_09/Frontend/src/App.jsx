4e838ddb51474767ace5aa3ac11b1eda684ab91a
Phạm Khôi Nguyên <khoinguyenpkn@gmail.com>
2025-09-29 23:51:12 +0700

    Add files via upload

---
+++ WebUIT_23_09/Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://youtu.be/fOQ_-gZsnYQ?t=136" target="_blank">
+          <div>Hello World!</div>
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default App
